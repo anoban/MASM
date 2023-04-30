987a3f1eb789731dc6a73a47383d2b6270a3ab9b
anoban <2016sb117@univ.jfn.ac.lk>
2023-04-30 11:03:35 +0530

    renamed:    MSVC Intrinsics/Chapter 02/Example - 01/Ch02_01.c -> MSVC Intrinsics/Chapter 02/Ch02_01.c 	renamed:    MSVC Intrinsics/Chapter 02/Example - 01/xmmval.h -> MSVC Intrinsics/Chapter 02/xmmval.h 	modified:   clean.ps1

--- clean.ps1
+++ clean.ps1
@@ -1,5 +1,5 @@
-Remove-Item *.exe
-Remove-Item *.obj	
-Remove-Item *.pdb
-Remove-Item *.ilk
-Remove-Item *.i     
+Get-ChildItem -Include *.exe -Recurse | Remove-Item	
+Get-ChildItem -Include *.obj -Recurse | Remove-Item	
+Get-ChildItem -Include *.pdb -Recurse | Remove-Item
+Get-ChildItem -Include *.ilk -Recurse | Remove-Item
+Get-ChildItem -Include *.i   -Recurse | Remove-Item	  
