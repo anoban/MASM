9631374af7f04ddbb97cb4542a9a9041c8de6fd8
anoban <2016sb117@univ.jfn.ac.lk>
2023-07-11 11:29:17 +0530

    modified:   chap2_ex1.c

--- chap2_ex1.c
+++ chap2_ex1.c
@@ -2,14 +2,35 @@
 #include <sal.h>
 #include "packed.h"
 
-static void __forceinline AddI16_AVX(const xmm_t* _In_ arr_packed_0, const xmm_t* _In_ arr_packed_1,
-    xmm_t* _Out_ sat_sum, xmm_t* _Out_ flpar_sum) {
+static void inline AddI16_AVX(_In_ const xmm_t* arr_packed_0, _In_ const xmm_t* arr_packed_1,
+    _Out_ xmm_t* sat_sums, _Out_ xmm_t* flpar_sums) {
 
     __m128i xmm_arr0 = _mm_load_si128((const __m128i*) arr_packed_0);
     __m128i xmm_arr1 = _mm_load_si128((const __m128i*) arr_packed_1);
 
     __m128i xmm_fliparound_sum = _mm_add_epi16(xmm_arr0, xmm_arr1);
     __m128i xmm_saturated_sum = _mm_adds_epi16(xmm_arr0, xmm_arr1);
+
+    _mm_store_si128((__m128i*) flpar_sums, xmm_fliparound_sum);
+    _mm_store_si128((__m128i*) sat_sums, xmm_saturated_sum);
+
+    return;
+}
+
+
+static void inline AddU16_AVX(_In_ const xmm_t* arr_packed_0, _In_ const xmm_t* arr_packed_1,
+    _Out_ xmm_t* sat_sums, _Out_ xmm_t* flpar_sums) {
+
+    __m128i xmm_arr0 = _mm_load_si128((const __m128i*) arr_packed_0);
+    __m128i xmm_arr1 = _mm_load_si128((const __m128i*) arr_packed_1);
+
+    // NOTE:
+    // There's no packed unsigned equivalent for _mm_add_epi16.
+    __m128i xmm_fliparound_sum = _mm_add_epi16(xmm_arr0, xmm_arr1);
+    __m128i xmm_saturated_sum = _mm_adds_epu16(xmm_arr0, xmm_arr1);
+
+    _mm_store_si128((__m128i*) flpar_sums, xmm_fliparound_sum);
+    _mm_store_si128((__m128i*) sat_sums, xmm_saturated_sum);
 
     return;
 }
